717d529c46bfce005488dd7608921d3c5ce27015
Leslie Mares <lesliemares@Leslies-MacBook-Pro.local>
2021-04-28 18:56:24 -0700

    ingredients model complete

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,24 @@ from django.db import models
 # Import the User
 from django.contrib.auth.models import User
 
+UNITS = (
+    ('gm', 'Grams'),
+    ('cups', 'Cups'),
+    ('items', 'Each'),
+    ('oz', 'Ounces'),
+    ('Tbsp', 'Tablespoons'),
+    ('tsp', 'Teaspoons'),
+    ('lbs', 'Pounds'),
+    ('gallon', 'Gallon'),
+    ('ml', 'Milliliters'),
+    ('pint', 'Pint'),
+)
+
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-    
+    amount = models.FloatField()
+    unit = models.models.CharField(max_length=1, choices=UNITS, default=UNITS[0][0])
+
+    def __str__(self):
+        return f"{self.name}: {self.amount}{self.unit}"
