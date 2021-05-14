727353aa67a07a26a1a0ad83c3b4e97d96296d2b
Raul Rodriguez <raul.i.rodriguez@gmail.com>
2021-05-13 23:33:38 -0500

    updated MealForm for calendar

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from .models import Ingredient, Meal
 
 
@@ -11,3 +11,15 @@ class MealForm(ModelForm):
     class Meta:
         model = Meal
         fields = ['date', 'meal']
+
+        widgets = {
+            'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+            'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+        }
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(MealForm, self).__init__(*args, **kwargs)
+        # input_formats to parse HTML5 datetime-local input to datetime field
+        self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
+        self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
